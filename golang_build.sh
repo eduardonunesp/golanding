7fb6be5e24114d01a42f86a9ed78da86735469fc
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2016-10-19 09:00:02 -0200

    Paths and template

--- golang_build.sh
+++ golang_build.sh
@@ -1,1 +1,4 @@
-docker run --rm -it -v /Users/eduardonunesp/Developer/go:/gopath -v /Users/eduardonunesp/Developer/go/src/github.com/eduardonunesp/golanding:/app -e GOPATH=/gopath -w /app golang:1.7.1 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags=-s -o main'
+#!/bin/bash
+go_version=1.7.1
+
+docker run --rm -it -v $GOPATH:/gopath -v $(pwd):/app -e GOPATH=/gopath -w /app golang:$go_version sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags=-s -o main'
--- main.go
+++ main.go
@@ -12,7 +12,18 @@ func main() {
 	router.Static("/assets", "./assets")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
+			"title":           "Golanding",
+			"subtitle":        "Your favorite landing page",
+			"sub_subtitle":    "In bootstrap :D",
+			"action_button":   "Let do it",
+			"about_title":     "About",
+			"services_title":  "Services",
+			"portfolio_title": "Portfolio",
+			"contact_title":   "Contact",
+			"contact_call":    "Let's Get In Touch!",
+			"contact_text":    "Ready to start",
+			"contact_phone":   "+55 11 1234 1234",
+			"contact_email":   "eduardonunesp@gmail.com",
 		})
 	})
 	router.Run(":3000")
